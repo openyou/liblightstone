0aa0885f480ae7434a414bb4fb77004bd7e4de3c
Kyle Machulis <qdot@nonpolynomial.com>
2008-01-06 03:24:38 +0000

    Fixed lightstone test to have common error codes between windows and libhid

--- examples/lightstone_test/main.c
+++ examples/lightstone_test/main.c
@@ -6,16 +6,26 @@ int main(int argc, char** argv)
 	lightstone test = NULL;
 	lightstone_info r;
 	int ret;
+
+    //Uncomment for libhid debug messages
+	//#ifdef USE_LIBHID
+	//hid_set_debug(HID_DEBUG_ALL);
+	//hid_set_debug_stream(stderr);
+	//hid_set_usb_debug(0);
+	//#endif USE_LIBHID
+	
 	ret = lightstone_get_count(&test, 0);
+
 	if(!ret)
 	{
-		printf("No lightstones connected!");
+		printf("No lightstones connected!\n");
 		return 1;
 	}
+	printf("Found %d Lightstones\n", ret);
 	ret = lightstone_open(&test, 0);
-	if(!ret)
+	if(ret < 0)
 	{
-		printf("Cannot open lightstone!");
+		printf("Cannot open lightstone!\n");
 		return 1;
 	}
 	while(1)
